2f1294dace2a3e4abf16317566202ca5b2a4c4c5
STIKI WEB2 <arthawebid@gmail.com>
2021-03-18 22:34:23 +0800

    Update System 01
    Signed-off-by: STIKI WEB2 <arthawebid@gmail.com>

--- timmer_mesin_cuci_03.ino
+++ timmer_mesin_cuci_03.ino
@@ -2,10 +2,17 @@ int Relay = 12;
 int End = 0;
 int Lmp = 2;
 int btn1 = 4;
-int posON;
-int cx = 0;
+int stt;
+int jeda=480000;
 
 void setup() {
+  
+  stt=1;
+  //debug mode
+  Serial.begin(9600);
+  Serial.println("Pengering: Starting...");
+  //debug mode
+  
   pinMode(Relay, OUTPUT);
   pinMode(Lmp,OUTPUT);
   digitalWrite(Relay, HIGH);
@@ -25,26 +32,30 @@ void setup() {
   delay(500);
   digitalWrite(Lmp, LOW);
   pinMode(btn1,INPUT);
+  Serial.println("Pengering: Ready");
 }
 void loop()
 {
     if(digitalRead(btn1) == HIGH){
-      if(posON==1){
+      if(stt==0){
+        Serial.println("Pengering: ON");
         digitalWrite(Lmp, HIGH);
         digitalWrite(Relay, LOW);
-        delay(5000);
-        posON=0;
+        //delay(480000);
+        delay(jeda);
+        stt=1;
+        Serial.println("Pengering: OFF");
         digitalWrite(Relay, HIGH);
         digitalWrite(Lmp, LOW);
       }
     }else{
-      if(posON==0){
-        digitalWrite(Lmp, HIGH);
-        digitalWrite(Relay, LOW);
-        delay(5000);
-        posON=1;
+      if(stt==1){
         digitalWrite(Relay, HIGH);
         digitalWrite(Lmp, LOW);
+        stt=0;
+        delay(500);
+        Serial.println("Pengering: Reset-Ready");
       }
     }
+    
 }
